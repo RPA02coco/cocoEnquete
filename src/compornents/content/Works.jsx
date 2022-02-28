b63c159775b1f71ed010918be5b2c9767323611d
RPA02coco <87287796+RPA02coco@users.noreply.github.com>
2022-02-28 14:23:46 +0900

    works修正

--- src/compornents/content/Works.jsx
+++ src/compornents/content/Works.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import styles from './Works.module.css';
 import CheckBox from '../CheckBox/CheckBox';
 
-const Works = ({form, setForm}) => {
+const Works = ({ form, setForm }) => {
   // const [checkedItemsWorks, setCheckedItemsWorks] = useState([]);
   // const [checkedItemsNenshu, setCheckedItemsNenshu] = useState([]);
   const workLists = [
@@ -11,35 +11,35 @@ const Works = ({form, setForm}) => {
     '医師', '弁護士', '会計士・税理士', '弁理士', 'アルバイト・パート', '主婦・主夫',
     '学生', '無職', 'その他',
   ];
-  let chkWorkList = [];
-
   const annualIncomeLists = [
     '～300万円', '～400万円', '～500万円', '～600万円', '～700万円', '～800万円',
     '～900万円', '～1,000万円', '～1,100万円', '～1,200万円', '～1,300万円', '～1,400万円',
     '～1,500万円', '～2,000万円', '～2,001万円', '収入無し',
   ];
-  const chkAnnualIncomeLists = [];
 
   const changeHandler = (e) => {
     const name = e.target.name;
+    // let arrayName = chkWorkList;
+    // if (name === 'annualIncome') {
+    //   arrayName = chkAnnualIncomeLists;
+    // }
 
     setForm((prev) => {
+      let chkWorkList = prev[name].value;
       console.log('e.target.value', e.target.value);
-      console.log('chkWorkList', chkWorkList);
-      // e.target.valueをはいれつかするしょりをここに入れる
+      console.log('chkWorkList', chkWorkList, chkWorkList.includes(e.target.value));
+      // e.target.valueを配列化する処理をここに入れる
       // バグあり要修正
-      if (chkWorkList = chkWorkList.includes(e.target.value)) {
-        chkWorkList.filter((item) => item !== e.target.value);
+      if (chkWorkList.includes(e.target.value)) {
+        chkWorkList = chkWorkList.filter((item) => item !== (e.target.value));
       } else {
-        chkWorkList.concat(e.target.value);
+        chkWorkList = chkWorkList.concat(e.target.value);
       }
-
-      // returnの中の、valueに設定している値を、↑で設定した配列に変更する
-      console.log(chkWorkList);
+      console.log('setForm内', chkWorkList);
 
       return {
         ...prev,
-        [name]: {...prev[name], value: chkWorkList},
+        [name]: { ...prev[name], value: chkWorkList },
       };
     });
   };
@@ -52,17 +52,16 @@ const Works = ({form, setForm}) => {
       <p className={styles.subTitle}>ご職業:</p>
       <div className={styles.workHeader}>
         {workLists.map((item, index) => {
-          index = index + 1;
           return (
-            <div className={styles.workitem} key={`key_${index}`}>
+            <div className={styles.workitem} key={`key_works${item}`}>
               <CheckBox
-                id={`id_${index}`}
+                id={`id_works${index}`}
                 value={item}
                 name={'works'}
-                checked={chkWorkList[index-1]}
+                checked={form.works.value.includes(item)}
                 onChange={changeHandler}
               />
-              <label htmlFor={`id_${index}`}>
+              <label htmlFor={`id_works${index}`}>
                 {item}
               </label>
             </div>
@@ -73,17 +72,16 @@ const Works = ({form, setForm}) => {
       <p className={styles.subTitle}>ご年収:</p>
       <div className={styles.workHeader}>
         {annualIncomeLists.map((item, index) => {
-          index = index + 1;
           return (
-            <div className={styles.workitem} key={`key_${index}`}>
+            <div className={styles.workitem} key={`key_annualIncome${item}`}>
               <CheckBox
-                id={`id_${index}`}
+                id={`id_annualIncome${index}`}
                 value={item}
                 name={'annualIncome'}
-                checked={chkAnnualIncomeLists[index-1]}
+                checked={form.annualIncome.value.includes(item)}
                 onChange={changeHandler}
               />
-              <label htmlFor={`id_${index}`}>
+              <label htmlFor={`id_annualIncome${index}`}>
                 {item}
               </label>
             </div>
