03d62fb8165400d1c0d0e5512c6a7865906ee506
RPA02coco <87287796+RPA02coco@users.noreply.github.com>
2022-03-22 13:58:29 +0900

    update

--- src/compornents/Button/BackNextButton.jsx
+++ src/compornents/Button/BackNextButton.jsx
@@ -1,10 +1,13 @@
 import PropTypes from 'prop-types';
-import { useTheme } from '@emotion/react';
+import { useTheme } from '@mui/material/styles';
 import { MobileStepper, Button, Typography } from "@mui/material"
 import { KeyboardArrowLeft, KeyboardArrowRight } from "@mui/icons-material"
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 const BackNextButton = ({ handleNext, handleBack, activeStep, disableflg }) => {
   const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm')); // 画面サイズがsm以下か判定, 以上の場合はdownをupに変える
+  console.log('matches', isMobile);
 
   const labelProps = {};
   if (disableflg) {
@@ -15,33 +18,41 @@ const BackNextButton = ({ handleNext, handleBack, activeStep, disableflg }) => {
     );
     labelProps.error = true;
   }
-  
+
   return (
     <MobileStepper
       variant="dots"
       steps={7}
       position="bottom"
       activeStep={activeStep}
-      sx={{ maxWidth: 400, flexGrow: 1 }}
+      sx={{
+        maxWidth: 600,
+        left: isMobile ? "" : "50%",
+        transform: isMobile ? "" : "translate(-50%, 0)",
+      }}
       nextButton={
-        <Button size="small" onClick={handleNext} disabled={(activeStep === 6) || (disableflg === true)}> 
+        < Button size="small" onClick={handleNext} disabled={(activeStep === 6) || (disableflg === true)}>
           Next
-          {theme.direction === 'rtl' ? (
-            <KeyboardArrowLeft />
-          ) : (
-            <KeyboardArrowRight />
-          )}
-        </Button>
+          {
+            theme.direction === 'rtl' ? (
+              <KeyboardArrowLeft />
+            ) : (
+              <KeyboardArrowRight />
+            )
+          }
+        </Button >
       }
       backButton={
-        <Button size="small" onClick={handleBack} disabled={(activeStep === 0) || disableflg}>
-          {theme.direction === 'rtl' ? (
-            <KeyboardArrowRight />
-          ) : (
-            <KeyboardArrowLeft />
-          )}
+        < Button size="small" onClick={handleBack} disabled={(activeStep === 0) || disableflg}>
+          {
+            theme.direction === 'rtl' ? (
+              <KeyboardArrowRight />
+            ) : (
+              <KeyboardArrowLeft />
+            )
+          }
           Back
-        </Button>
+        </Button >
       }
     />
   )
