729bcd847352d602608fa1ee3ac2545f4f540568
RPA02coco <87287796+RPA02coco@users.noreply.github.com>
2022-04-26 10:30:54 +0900

    使用するDatePickerの変更

--- src/compornents/Input/DateSelect.jsx
+++ src/compornents/Input/DateSelect.jsx
@@ -1,17 +1,19 @@
 import PropTypes from 'prop-types';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import { ja } from 'date-fns/locale';
-import { DatePicker, LocalizationProvider } from "@mui/lab"
+import { LocalizationProvider, DesktopDatePicker } from "@mui/lab"
 import { TextField } from '@mui/material';
 import { errorViewer, errTextViewer } from './TextBox';
 
 const DateContent = ({ form, tgtName, onChange }) => {
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns} locale={ja}>
-      <DatePicker className='inputdates'
+      <DesktopDatePicker
+        // <DatePicker
+        disableFuture
+        orientation="portrait"
         label={form[tgtName].label}
         value={form[tgtName].value}
-        defaultValue={form[tgtName].value === '' ? '' : form[tgtName].value}
         mask="____年__月__日"
         inputFormat="yyyy年MM月dd日"
         views={['year', 'month', 'day']}
